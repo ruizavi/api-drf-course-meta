aade645c0640fc51ff7cea0d976b913bb49e9708
avi <avi.ruiz@proton.me>
2023-06-20 17:58:21 -0600

    change in authorization conditions for categories

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.exceptions import PermissionDenied
 from rest_framework import generics
 from .models import Category, MenuItem
@@ -12,23 +12,26 @@ class CategoriesView(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
 
     def get_permissions(self):
+        isAdmin = self.request.user.is_superuser
         gr = self.request.user.groups.filter(name='Manager').exists()
-        if (not gr):
-            raise PermissionDenied()
+        if (gr or isAdmin):
+            return [IsAuthenticated()]
 
-        return [IsAuthenticated()]
+        raise PermissionDenied()
 
 
 class SingleCategoryView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUser]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
     def get_permissions(self):
+        isAdmin = self.request.user.is_superuser
         gr = self.request.user.groups.filter(name='Manager').exists()
-        if (not gr):
-            raise PermissionDenied()
+        if (gr or isAdmin):
+            return [IsAuthenticated()]
 
-        return [IsAuthenticated()]
+        raise PermissionDenied()
 
 
 class MenuItemsView(generics.ListCreateAPIView):
@@ -51,11 +54,12 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MenuItemSerializer
 
     def get_permissions(self):
+        isAdmin = self.request.user.is_superuser
+        gr = self.request.user.groups.filter(name='Manager').exists()
+        if (gr or isAdmin):
+            return [IsAuthenticated()]
+
         if (self.request.method == 'GET'):
             return []
 
-        gr = self.request.user.groups.filter(name='Manager').exists()
-        if (not gr):
-            raise PermissionDenied()
-
-        return [IsAuthenticated()]
+        raise PermissionDenied()
